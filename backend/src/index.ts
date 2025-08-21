2c45b32b7c392abf1f5a5943b61fc3b8e636023e
VitaliaOrmanzhy <vormanzi812@gmail.com>
2025-08-21 16:13:52 +0300

    cors added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import "dotenv/config"
 import router from "./routes";
 import connectToDB from "./db/db";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({ origin: "http://localhost:5174" }));
 app.use(express.json());
 app.use("/api", router);
 
