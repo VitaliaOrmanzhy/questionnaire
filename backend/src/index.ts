87d9196d9f0731cc4252da928f874839b8063326
VitaliaOrmanzhy <vormanzi812@gmail.com>
2025-08-22 03:02:27 +0300

    added cors methods

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,8 +7,9 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: "http://localhost:5174" }));
+app.use(cors({ origin: "http://localhost:5173", methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], optionsSuccessStatus: 200, }));
 app.use(express.json());
+
 app.use("/api", router);
 
 connectToDB();
