aa47df77c07ba7c345a485a47024520de49d4ff1
VitaliaOrmanzhy <vormanzi812@gmail.com>
2025-09-02 18:32:07 +0300

    removed completionsCount, added completions for quiz, getAverageCompletionsTime method

--- backend/src/models/quiz.ts
+++ backend/src/models/quiz.ts
@@ -12,18 +12,24 @@ export interface IQuestion {
   options: IOption[];
 }
 
+export interface ICompletion {
+  milliseconds: number;
+  score: number;
+  completorId: Types.ObjectId;
+}
+
 export interface IQuiz {
   title: string;
   description: string;
   questions: IQuestion[];
   authorId: Types.ObjectId;
-  completionsCount: number;
-  averageCompletionTime?: number;
+  completions: ICompletion[];
 }
 
 export interface IQuizMethods {
   getQuestionsCount(): number;
-  increaseCompletionsCount(): number;
+  getCompletionsCount(): number;
+  getAverageCompletionTime(): number;
 }
 
 const optionSchema = new mongoose.Schema<IOption>({
@@ -46,6 +52,18 @@ const questionSchema = new mongoose.Schema<IQuestion>({
   options: [optionSchema],
 });
 
+const completionSchema = new mongoose.Schema<ICompletion>({
+  milliseconds: {
+    type: Number,
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+  completorId: Schema.Types.ObjectId,
+});
+
 const quizSchema = new mongoose.Schema<IQuiz, Model<IQuiz>, IQuizMethods>(
   {
     title: {
@@ -62,12 +80,10 @@ const quizSchema = new mongoose.Schema<IQuiz, Model<IQuiz>, IQuizMethods>(
       ref: "User",
       required: true,
     },
-    completionsCount: {
-      type: Number,
-      default: 0,
-      required: true,
+    completions: {
+      type: [completionSchema],
+      default: [],
     },
-    averageCompletionTime: Number,
   },
   {
     timestamps: true,
@@ -78,8 +94,14 @@ quizSchema.methods.getQuestionsCount = function () {
   return this.questions.length;
 };
 
-quizSchema.methods.increaseCompletionsCount = function () {
-  return ++this.completionsCount;
+quizSchema.methods.getCompletionsCount = function () {
+  return this.completions.length;
+};
+
+quizSchema.methods.getAverageCompletionTime = function () {
+  return this.completions.reduce((acc, el, _, arr) => {
+    return acc + el.milliseconds / arr.length;
+  }, 0);
 };
 
 export const Quiz = mongoose.model("Quiz", quizSchema);
